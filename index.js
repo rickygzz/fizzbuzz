0b2c175a29771958cba03ad0501a4e3a42d8cd6a
Ricardo González Garza <rickygzz@hotmail.com>
2022-05-05 13:21:43 -0500

    Refactor into FizzbuzzService. Updated index.js to test it.

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ filtered = ExplorerService.filterByMission(explorers, "node");
 amount = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
 usernames = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
 
-console.log(filtered);
-console.log(amount);
-console.log(usernames);
+// console.log(filtered);
+// console.log(amount);
+// console.log(usernames);
+
+const FizzbuzzService = require("./lib/services/FizzbuzzService")
+
+
+const explorer1 = {name: "Explorer1", score: 1}
+FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
+
+const explorer3 = {name: "Explorer3", score: 3}
+FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+
+const explorer5 = {name: "Explorer5", score: 5}
+FizzbuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+
+const explorer15 = {name: "Explorer15", score: 15}
+FizzbuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+
+console.log(explorer1);
+console.log(explorer3);
+console.log(explorer5);
+console.log(explorer15);
