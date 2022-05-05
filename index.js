9d88775f062b4a3e3fbd850b7da78e2c804f3bec
Ricardo González Garza <rickygzz@hotmail.com>
2022-05-05 13:14:20 -0500

    Refactor into ExplorerService.js. Updated index.js to test it.

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ const Reader = require("./lib/utils/Reader");
 const explorers = Reader.readJsonFile("explorers.json");
 // esto regresa la lista de explorers del archivo
 
-console.log(explorers);
+// console.log(explorers);
+
+const ExplorerService = require("./lib/services/ExplorerService")
+
+// Aplicación del ExplorerService sobre la lista de explorers
+filtered = ExplorerService.filterByMission(explorers, "node");
+amount = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+usernames = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+
+console.log(filtered);
+console.log(amount);
+console.log(usernames);
